f754c7b1e46c2a31c1a8086618d2427196e790a0
jjh <goodgirl0118@naver.com>
2021-11-04 15:28:27 +0900

    💻 Portfolio

---
+++ JJH/js/index.js
@@ -0,0 +1,68 @@
+'use-strict';
+
+// Make navbar transperent whin it is on the top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  if(window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
+});
+
+// navbar toggle click open menu
+const navbarToggleBtn = document.querySelector('.navbar_toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar_menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if(link == null) {
+    return;
+  }
+  navbarMenu.classList.remove('open');
+
+  // console.log(event.target.dataset.link);
+  scrollIntoView(link);
+}); 
+
+// Handle click on 'contact me' button on home
+const homeContactBtn = document.querySelector('.home_contact');
+homeContactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+});
+
+// Make home slowly fade to transperent as the window scrolls down
+const home = document.querySelector('.home_container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// show arrow up button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if(window.scrollY > homeHeight /2) {
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
+  }
+});
+
+// arrow-up click up!!
+arrowUp.addEventListener('click', () => {
+  scrollIntoView('#home');
+});
+
+
+
+// common scroll function
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
